a23b3c346239acdf930c05d9ba0b620d9447d06b
K ken <kstory8715@gmail.com>
2019-08-18 23:32:38 +0900

    이벤트 루프 case 문으로 변경함.

--- src/components/Commons/PlayerIpc/index.vue
+++ src/components/Commons/PlayerIpc/index.vue
@@ -16,18 +16,30 @@ export default {
           const eventName = arg[0];
           const eventValue = arg[1];
           if (eventName === "onStateChange") {
-            if (eventValue === -1) {
-              this.$log.info(eventName, `${eventValue} - (시작되지 않음)`);
-            } else if (eventValue === 0) {
-              this.$log.info(eventName, `${eventValue} - (종료)`);
-            } else if (eventValue === 1) {
-              this.$log.info(eventName, `${eventValue} - (재생 중)`);
-            } else if (eventValue === 2) {
-              this.$log.info(eventName, `${eventValue} - (일시 중지)`);
-            } else if (eventValue === 3) {
-              this.$log.info(eventName, `${eventValue} - (버퍼링)`);
-            } else if (eventValue === 5) {
-              this.$log.info(eventName, `${eventValue} - (동영상 신호)`);
+            // FIXME: 정확한 이벤트 구분을 위해 모두 작성.
+            // 1. 영상을 처음 재생했을 경우
+            //   * -1 -> 3 -> 1
+            // 2. 영상을 재생중에 다른 영상을 재생할경우
+            //   * -2 -> -1 -> 3 -> 1
+            switch (eventValue) {
+              case -1:
+                this.$log.info(eventName, `${eventValue} - (시작되지 않음)`);
+                break;
+              case 0:
+                this.$log.info(eventName, `${eventValue} - (종료)`);
+                break;
+              case 1:
+                this.$log.info(eventName, `${eventValue} - (재생 중)`);
+                break;
+              case 2:
+                this.$log.info(eventName, `${eventValue} - (일시 중지)`);
+                break;
+              case 3:
+                this.$log.info(eventName, `${eventValue} - (버퍼링)`);
+                break;
+              case 5:
+                this.$log.info(eventName, `${eventValue} - (동영상 신호)`);
+                break;
             }
           } else if (eventName === "currentTime") {
             this.$log.info(eventName, eventValue);
